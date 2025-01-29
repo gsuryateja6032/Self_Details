17c889ca755d5e1ee3f0f701bcb66c8097b226f9
GajavellySuryaTeja <gsuryateja6032@gmail.com>
2025-01-29 15:07:05 +0530

    first commit

---
+++ src/test/java/self_details/Class1.java
@@ -0,0 +1,13 @@
+package self_details;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("Name:Surya Teja");
+		System.out.println("Graduation:Btech");
+		System.out.println("Location:Hyderabad");
+		System.out.println("Profession:Software Engineer");
+
+	}
+
+}
