0972025ad8f343d7a3d8c45501d4b87bb35d8670
Malleshwari4 <pallikondamalleshwari@gmail.com>
2025-01-29 15:30:46 +1300

    added details

---
+++ src/test/java/self_details/Malleshwari.java
@@ -0,0 +1,12 @@
+package self_details;
+
+public class Malleshwari {
+
+	public static void main(String[] args) {
+		System.out.println("Name: malleshwari");
+		System.out.println("Graduation:degree");
+		System.out.println("Location:Nizamabad");
+		
+	}
+
+}
